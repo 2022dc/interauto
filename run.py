d262a4be1c6f633a24057dabd56911219f2d9bbc
dc-gu <1055239689@qq.com>
2022-06-24 14:26:04 +0800

    generate junitxml file

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import pytest
 import subprocess
 if __name__ == '__main__':
-    pytest.main(["-vs","testcase","--alluredir","reports/json"])
+    pytest.main(["-vs","testcase","--alluredir","reports/json","--junitxml=reports/result.xml"])
     subprocess.run("allure generate ./reports/json -o ./reports/html --clean",shell=True)
